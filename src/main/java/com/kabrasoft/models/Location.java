88567b37faf771c4b51a0a8e5cbfbf14013ffa44
ombasaMukhwami <mukhwami@gmail.com>
2023-08-07 16:29:43 +0300

    added Location model

---
+++ src/main/java/com/kabrasoft/models/Location.java
@@ -0,0 +1,15 @@
+package com.kabrasoft.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Location extends BaseModel{
+    private int animalId;
+    private String location;
+    private String timestamp;
+    private String rangerName;
+}
