251c4f90387812c0c83c11e0632c48b247d1f3cd
ombasaMukhwami <mukhwami@gmail.com>
2023-08-07 21:36:42 +0300

    implement the spark ui

--- src/main/java/com/kabrasoft/Main.java
+++ src/main/java/com/kabrasoft/Main.java
@@ -1,7 +1,89 @@
 package com.kabrasoft;
 
+import com.kabrasoft.configs.DbConfig;
+import com.kabrasoft.models.Animal;
+import com.kabrasoft.models.Sighting;
+import com.kabrasoft.services.AnimalService;
+import com.kabrasoft.services.SightingService;
+import com.kabrasoft.services.impl.AnimalServiceImpl;
+import com.kabrasoft.services.impl.SightingServiceImpl;
+import com.kabrasoft.utils.Helper;
+import org.sql2o.Sql2o;
+
+import java.util.HashMap;
+
+import static spark.Spark.staticFileLocation;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+        Sql2o sql2o = DbConfig.getDatabaseProduction();
+        AnimalService animalService = new AnimalServiceImpl(sql2o);
+        SightingService sightingService = new SightingServiceImpl(sql2o);
+        get("/", (req, res) -> {
+            return Helper.render(new HashMap<>(), "index.hbs");
+        });
+        get("/wildlife/add", (req, res) -> {
+            return Helper.render(new HashMap<>(), "wildlife-details.hbs");
+        });
+        get("/animal/add", (req, res) -> {
+            return Helper.render(new HashMap<>(), "animal-details.hbs");
+        });
+
+        post("/create-sighting", (req, res) -> {
+            String id = req.queryParams("id");
+            String location = req.queryParams("location");
+            String rangerName = req.queryParams("rangername");
+
+            try {
+                if (id == null || id.length() == 0
+                        || location == null || location.length() == 0 || rangerName == null || rangerName.length() == 0) {
+                    throw new IllegalArgumentException("invalid input all fields have to be provided");
+                }
+                int latestId = Integer.valueOf(id);
+                Animal animal = animalService.findAnimalById(latestId);
+                Sighting sightings = new Sighting(animal.getId(), location, rangerName);
+
+                //save sighting
+                sightingService.createSighting(sightings);
+
+            } catch (Exception e) {
+
+                System.out.print(e.getMessage());
+            }
+
+            res.redirect("/");
+            return null;
+        });
+
+        post("/create-animal", (req, res) -> {
+            Integer category = Integer.parseInt(req.queryParams("category"));
+            String name = req.queryParams("name");
+            String health = req.queryParams("health");
+            String age = req.queryParams("age");
+
+            try {
+                if (category == null || name == null || name.length() == 0 || health == null || health.length() == 0 || age == null || age.length() == 0) {
+                    System.out.print(category);
+                    System.out.print(name);
+                    System.out.print(health);
+                    System.out.print(age);
+                    throw new IllegalArgumentException("invalid input all fields have to be provided");
+                }
+
+                Animal animals = new Animal(category, name, health, age);
+                animalService.createAnimal(animals);
+
+            } catch (Exception e) {
+
+                System.out.print(e.getMessage());
+            }
+
+            res.redirect("/");
+            return null;
+        });
     }
 }
