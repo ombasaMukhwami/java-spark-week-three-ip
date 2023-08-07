00a44cae29fc9a7c9d7ded8a6b5d151cc87c399e
ombasaMukhwami <mukhwami@gmail.com>
2023-08-07 16:29:35 +0300

    added ranger model

---
+++ src/main/java/com/kabrasoft/models/Ranger.java
@@ -0,0 +1,12 @@
+package com.kabrasoft.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Ranger extends BaseModel{
+    private String name;
+}
